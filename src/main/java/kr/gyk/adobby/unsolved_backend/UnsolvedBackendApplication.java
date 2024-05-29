c137f886f31f1379dbfa808eb360c68c04653d47
Giyu Kim <giyu1020@gmail.com>
2024-05-29 20:22:51 +0900

    Create Project

---
+++ src/main/java/kr/gyk/adobby/unsolved_backend/UnsolvedBackendApplication.java
@@ -0,0 +1,13 @@
+package kr.gyk.adobby.unsolved_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UnsolvedBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UnsolvedBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/kr/gyk/adobby/unsolved_backend/UnsolvedBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.gyk.adobby.unsolved_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UnsolvedBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
